f400f39a1090bf1ae3a17d3f288e9f3069d40a32
SantiagoSM2000 <santiagosanmartin2000@gmail.com>
2022-08-18 17:22:31 -0300

    Comienzo del BackEnd

---
+++ BackEnd/src/test/java/com/portfolio/SM/SmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.SM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
